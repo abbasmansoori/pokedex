a7d688246bf3d70ca59b17c6fe188f4701db3ff2
abbasmansoori <abbas.mansoori@hotmail.com>
2021-11-21 20:49:44 +0100

    first commit, created single pokecard

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+import Pokecard from './Pokecard';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>
+          <Pokecard
+          id={4}
+          name="charmander"
+          type="fire"
+          exp="62"
+          />
+          </h1>
+      </div>
+    );
+  }
+    
+}
+
+
+
+
+
+export default App;
